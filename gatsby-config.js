eeb4ce619cb62fc7afe8941e7b59fd0c4c1455d5
Dolev Hadar <dolevh@wix.com>
2021-08-13 19:39:53 +0300

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,32 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Personal Website",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Personal Website',
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-prettier-eslint',
+      options: {
+        prettier: {
+          patterns: [
+            '**/*.{css,scss,less}',
+            '**/*.{json,json5}',
+            '**/*.{graphql}',
+            '**/*.{md,mdx}',
+            '**/*.{html}',
+            '**/*.{yaml,yml}',
+          ],
+        },
+        eslint: {
+          patterns: '**/*.{js,jsx,ts,tsx}',
+          customOptions: {
+            fix: true,
+            cache: true,
+          },
+        },
+      },
+    },
+  ],
 };
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import '../css/index.scss';
+
+const IndexPage = (): React.ReactNode => {
+  return (
+    <main className="root">
+      <title>Dolev Hadar</title>
+      <h1 className="title">Dolev Hadar</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
