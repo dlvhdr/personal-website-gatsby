bc22b40e3361552beae538c08d782fc3d40d3664
Dolev Hadar <dolevh@wix.com>
2021-08-13 19:47:49 +0300

    initial content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,22 @@ const IndexPage = (): React.ReactNode => {
     <main className="root">
       <title>Dolev Hadar</title>
       <h1 className="title">Dolev Hadar</h1>
+      <div className="description">
+        <p>💻 A Full stack engineer with 6+ years of experience.</p>
+        <p>
+          🌈 Front end oriented with extensive knowledge in JavaScript,
+          Typescript, React, React Native, GraphQL, Relay, Redux, JSS, Sass and
+          Flow
+        </p>
+        <p>
+          ⚙️ Backend skills include knowledge in PHP (Hack), Go, various ORMs,
+          Docker, Linux, Git and Github, Mercurial, SQL, Bash
+        </p>
+        <p>
+          ✨ Self learner, can lead projects from planning to execution, team
+          player
+        </p>
+      </div>
     </main>
   );
 };
